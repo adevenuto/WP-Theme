a1f7e78c3c6740cff73260e2be25a19aba0c3ef9
Anthony DeVenuto <adevenuto@macbook-m1.local>
2024-01-12 20:15:54 -0600

    added assets and enqued

--- wp-content/themes/experimentaltheme/functions.php
+++ wp-content/themes/experimentaltheme/functions.php
@@ -1,7 +1,13 @@
 <?php 
 
-function university_assets() {
-    wp_enqueue_style('university-style-sheet', get_stylesheet_uri());
+function experimental_theme_assets() {
+    wp_enqueue_script('main-js', get_theme_file_uri('/build/index.js'), ['jquery'], '1.0', true);
+    
+    wp_enqueue_style('google-fonts', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
+    wp_enqueue_style('font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
+    wp_enqueue_style('university-style-sheet', get_theme_file_uri('/build/style-index.css'));
+    wp_enqueue_style('university-style-sheet-index', get_theme_file_uri('/build/index.css'));
+    
 }
 
-add_action('wp_enqueue_scripts', 'university_assets');
+add_action('wp_enqueue_scripts', 'experimental_theme_assets');
