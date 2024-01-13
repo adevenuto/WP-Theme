7971d929070e72a92604e4c7490d6c4333640f5d
Anthony DeVenuto <adevenuto@macbook-m1.local>
2024-01-12 20:50:22 -0600

    adding links

--- wp-content/themes/experimentaltheme/functions.php
+++ wp-content/themes/experimentaltheme/functions.php
@@ -2,12 +2,17 @@
 
 function experimental_theme_assets() {
     wp_enqueue_script('main-js', get_theme_file_uri('/build/index.js'), ['jquery'], '1.0', true);
-    
     wp_enqueue_style('google-fonts', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
     wp_enqueue_style('font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
     wp_enqueue_style('university-style-sheet', get_theme_file_uri('/build/style-index.css'));
     wp_enqueue_style('university-style-sheet-index', get_theme_file_uri('/build/index.css'));
     
 }
+function theme_features() {
+  add_theme_support('title-tag');
+}
 
 add_action('wp_enqueue_scripts', 'experimental_theme_assets');
+
+
+add_action('after_setup_theme', 'theme_features');
